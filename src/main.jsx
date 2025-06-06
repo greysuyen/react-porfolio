b656c6d64c9bfc940c4c3b92e466fbcbd741ac66
greysuyen <carlambay687@gmail.com>
2025-06-07 01:21:21 +0800

    fixed homepage issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from "react-router-dom";
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
