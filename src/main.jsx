7f4ff2a5bc8f186dc4b13c53097acb36cbd6f62a
greysuyen <carlambay687@gmail.com>
2025-06-07 01:22:44 +0800

    homepage json final fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from "react-router-dom";
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
