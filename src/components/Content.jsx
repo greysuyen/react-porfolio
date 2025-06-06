28f3c2dd0b53981734011c7ac5e7d49c0563c93d
greysuyen <carlambay687@gmail.com>
2025-06-06 15:24:02 +0800

    initial commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,7 @@ import './header.css'
 const Content = () => {
     return(
         <div className="content">
-            <p>Lorem ipsum dolor si amet consectetur adipisicing elit. Repellendus provident earum placeat accusantium, nisi ea dignissimos soluta odit voluptate, hic voluptas ipsum repudiandae. Aperiam accusantium, iusto labore accusamus iure laboriosam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus provident earum placeat accusantium, nisi ea dignissimos soluta odit voluptate, hic voluptas ipsum repudiandae. Aperiam accusantium, iusto labore accusamus iure laboriosam?</p>
         </div>
     );
 }
