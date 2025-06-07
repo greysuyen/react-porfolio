0979e872fb06b398b831b2cf102e98246ee57351
greysuyen <carlambay687@gmail.com>
2025-06-07 16:38:51 +0800

    vite config added base

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,17 +7,17 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <HashRouter>
+    <Router basename="/react-portfolio">
       <>
         <Profile />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </>
-    </HashRouter>
+    </Router>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ import '../styles/nav.css'
 function Navbar() {
   return (
     <nav className="nav">
-      <NavLink to="/">Home</NavLink> {" "}
-      <NavLink to="/about">About</NavLink> {" "}
+      <NavLink to="/">Home</NavLink> 
+      <NavLink to="/about">About</NavLink> 
       <NavLink to="/projects">Projects</NavLink>
     </nav>
   );
