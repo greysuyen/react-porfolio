6abbb5234a686558b07f30672bb05fe06049df4d
greysuyen <carlambay687@gmail.com>
2025-06-07 14:06:38 +0800

    added hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Profile from './components/Profile'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Profile from './components/Profile';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -7,19 +7,20 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <>
         <Profile />
-        <Navbar />  
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
       </>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
 export default App;
+
 
