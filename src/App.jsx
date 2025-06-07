c5092d004a6e53a1f0b783f30e6913f11ff26bec
greysuyen <carlambay687@gmail.com>
2025-06-07 16:42:25 +0800

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/react-portfolio">
       <>
         <Profile />
         <Navbar />
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/react-portfolio/', // <-- this must match your repo name
   plugins: [react()],
 });
